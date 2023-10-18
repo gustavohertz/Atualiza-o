9a08658cd61cd947d99026e9ddc29b285b4f096c
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-10-17 23:06:10 -0300

    Create Personagem.java

---
+++ Personagem.java
@@ -0,0 +1,15 @@
+public interface Personagem {
+    String getNome();
+
+    int getHitPoints();
+
+    void setHitPoints(int hitPoints);
+
+    int getForca();
+
+    int getDefesa();
+
+    int getAgilidade();
+
+    int getFatorDano();
+}
